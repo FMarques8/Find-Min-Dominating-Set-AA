c88edb4a62d6be36053fdfb46e24c5e986b991ba
Francisco Marques <franciscocmarques@ua.pt>
2022-12-06 16:04:25 +0000

    algorithm finished and results obtained

--- min_dominant_set_randomized.py
+++ min_dominant_set_randomized.py
@@ -50,7 +50,7 @@ def read_Graph(fname: str, form: str = 'SW', size_check: bool = True, name: str 
                     
     elif form == 'custom': #previous project graphs -> very tiny
         G = nx.read_gml(fname)
-        G['name'] = name
+        G.graph['name'] = name
         if size_check == True:
             if len(G.nodes) > 500:
                 return True
@@ -114,8 +114,8 @@ def random_nodes_min_dom_sets(G: nx.Graph, p: int = 20):
     for i in range(p):
         edges = [list(e) for e in G.edges]
         nodes = [v for v in G.nodes]
-        
         removed_nodes = []
+        
         while len(nodes) > 0: # Iterates over nodes until there are none left
             
             # as it randomly chooses nodes, there is no need to count the number of connections for each node
@@ -148,14 +148,14 @@ def random_nodes_min_dom_sets(G: nx.Graph, p: int = 20):
                 for edge in reversed(edges):
                     if edge[0] == adj_node or edge[1] == adj_node:
                         edges.remove(edge)
-
+        
         if ~any(node in D for node in removed_nodes for D in dom_sets) and sorted(removed_nodes) not in dom_sets:
             dom_sets.append(sorted(removed_nodes))
         else:
             i -= 1
             threshold +=1
             
-        if threshold >= 10000: #threshold in case new unique dom sets arent found
+        if threshold >= 50: #threshold in case new unique dom sets arent found
             reached_threshold = True
             break
     
@@ -223,11 +223,12 @@ def random_size_min_dom_sets(G: nx.Graph, p:int = 20):
         elif len(removed_nodes) == min_size: #if set has same size as current minimum set, add to list
             if ~any(node in D for node in removed_nodes for D in min_dom_sets) and sorted(removed_nodes) not in min_dom_sets:
                 min_dom_sets.append(sorted(removed_nodes))
+            
         else: #if size of set is greater than current minimum length, increment the threshold
             i -= 1
             threshold +=1
             
-        if threshold >= 10000: #threshold in case new unique dom sets arent found
+        if threshold >= 50: #threshold in case new unique dom sets arent found
             reached_threshold = True
             break
     
@@ -244,20 +245,22 @@ def nx_min_dom_sets(G:nx.Graph, p:int = 20):
     threshold = 0 # as this involves a while loop it is advised to use a threshold in case the algorithm gets stuck
     reached_threshold = False # variable to let us know if the threshold was reached or not
     
-    while len(minimum_dominating_sets) < p:
+    for i in range(p):
         random_set = random.sample(range(0,len(G)), random.randrange(1,len(G))) # creates random subset which will be tested
-        
         if nx.is_dominating_set(G, random_set):
             if len(random_set) < len(current_min_dominating_set): #if random set is smaller than previous dominating set, replace variables
                 current_min_dominating_set = random_set
                 minimum_dominating_sets = [current_min_dominating_set]
                 
             elif len(random_set) == len(current_min_dominating_set):
-                minimum_dominating_sets.append(random_set)
+                if ~any(node in D for node in random_set for D in minimum_dominating_sets) and sorted(random_set) not in minimum_dominating_sets:
+                    minimum_dominating_sets.append(random_set)
                 
-            else:
-                threshold += 1
-        if threshold > 10000:
+        else:
+            i = i + 1
+            threshold += 1
+            
+        if threshold > 50:
             reached_threshold = True
             break
         
@@ -276,27 +279,26 @@ graph_dir = {'custom_tinyG': 'custom', 'stanford': 'stanford', 'SW_Graphs': 'SW'
 
 G_list = [] # list with every graph, grouped by directory
 
-i = 0
 for dir in graph_dir.keys():
     lst = []
     with os.scandir(cwdir+"/"+dir) as itr:
         for entry in itr:
-            if entry.name.startswith('Grafo'): # skips some files that are not graphs found in the 'BD' folders
-                fname = cwdir+"/"+dir+"/"+entry.name
-                is_large = read_Graph(fname = fname, form = graph_dir[dir], size_check = True, name = dir+"/"+entry.name)
-                print(entry.name)
-                # skips very large graphs, a few will still be computed later but they are computationally heavy
-                # this saves alot of time
-                if is_large:
-                    print(fname+" is too large.")
-                    continue
-                else: # if graph is below certain size (500) appends it to list of graphs
-                    lst.append(read_Graph(fname = fname, form = graph_dir[dir], size_check = False, name = dir+"/"+entry.name))
-                    print(fname + " loaded.")
+            if entry.name.startswith('Experimentos'): # skips some specific files that are not graphs found in the 'BD' folders
+                continue
+            fname = cwdir+"/"+dir+"/"+entry.name
+            is_large = read_Graph(fname = fname, form = graph_dir[dir], size_check = True, name = dir+"/"+entry.name)
+            print(entry.name)
+            # skips very large graphs, a few will still be computed later but they are computationally heavy
+            # this saves alot of time
+            if is_large:
+                print(fname+" is too large.")
+                continue
+            else: # if graph is below certain size (500) appends it to list of graphs
+                lst.append(read_Graph(fname = fname, form = graph_dir[dir], size_check = False, name = dir+"/"+entry.name))
+                print(fname + " loaded.")
     G_list.append(lst)
     
 print("All graphs loaded.")
-
 
 ######
 ## Running the algorithms for the graphs and saving the results
@@ -322,7 +324,7 @@ for i in range(len(G_list)):
         f.write(list(graph_dir.keys())[i] + "/ graphs\n")
         f.write("#############################\n")
         f.write("\n")
-        for G in G_list[i][:15]:
+        for G in G_list[i]:
             f.write("Graph {G} with {n} nodes and {e} edges.\n".format(G = G.graph['name'], n = len(G.nodes), e = len(G.edges)))
             start = time.time()
             sets_min, threshold = random_nodes_min_dom_sets(G, p = 3)
@@ -341,7 +343,7 @@ for i in range(len(G_list)):
         f.write(list(graph_dir.keys())[i] + "/ graphs\n")
         f.write("#############################\n")
         f.write("\n")
-        for G in G_list[i][:15]:
+        for G in G_list[i]:
             f.write("Graph {G} with {n} nodes and {e} edges.\n".format(G = G.graph['name'], n = len(G.nodes), e = len(G.edges)))
             start = time.time()
             sets_min, threshold = random_size_min_dom_sets(G, p = 3)
@@ -360,7 +362,7 @@ for i in range(len(G_list)):
         f.write(list(graph_dir.keys())[i] + "/ graphs\n")
         f.write("#############################\n")
         f.write("\n")
-        for G in G_list[i][:15]:
+        for G in G_list[i]:
             f.write("Graph {G} with {n} nodes and {e} edges.\n".format(G = G.graph['name'], n = len(G.nodes), e = len(G.edges)))
             start = time.time()
             sets_min, threshold = nx_min_dom_sets(G, p = 3)
@@ -372,3 +374,117 @@ for i in range(len(G_list)):
             f.write("threshold reached: "+ str(threshold) + "\n")
             f.write("duration: "+ str(end-start) + "\n")
             f.write("\n")
+
+# Load some random large graphs
+
+large_graph_dir = {'stanford': 'stanford'}
+
+big_G_list = []
+
+# As we are purposedly loading large graphs, there is no need to verify the size first
+
+for dir in large_graph_dir.keys():
+    lst = []
+    dir_list = os.listdir(cwdir+"/"+dir)
+    graphs_read = []
+    
+    if len(dir_list) == 1: # if directory only has 1 graph (ex.: 'stanford'), there is no need to randomize the chosen graph
+        lst.append(read_Graph(fname = cwdir+"/"+dir+"/"+dir_list[0], form = large_graph_dir[dir], size_check=False, name = dir+"/"+dir_list[0]))
+        print(cwdir+"/"+dir+"/"+dir_list[0] + " loaded.")
+    else:
+        for i in range(3): # chooses 3 random graphs
+            k = random.randint(0, len(dir_list))
+            if k not in graphs_read:
+                lst.append(read_Graph(fname = cwdir+"/"+dir+"/"+dir_list[k], form = large_graph_dir[dir], size_check=False, name = dir+"/"+dir_list[k]))
+                print(cwdir+"/"+dir+"/"+dir_list[k] + " loaded.")
+                graphs_read.append(k)
+    big_G_list.append(lst)
+print("Large graphs loaded.")
+
+# # As the algorithm before didn't choose any graphs for these 3 folders, we can replace the text file with new data as they were empty
+
+for i in range(len(big_G_list)):
+    with open((list(large_graph_dir.keys())[i])+"_random_nodes_results.txt", 'w') as f:
+        f.write("#############################\n")
+        f.write(list(large_graph_dir.keys())[i] + "/ graphs\n")
+        f.write("#############################\n")
+        f.write("\n")
+        for G in big_G_list[i]:
+            f.write("Graph {G} with {n} nodes and {e} edges.\n".format(G = G.graph['name'], n = len(G.nodes), e = len(G.edges)))
+            start = time.time()
+            sets_min, threshold = random_nodes_min_dom_sets(G, p = 3)
+            end = time.time()
+            for D in sets_min:
+                for v in D:
+                    f.write(str(v)+",")
+                f.write("\n")
+            f.write("threshold reached: "+ str(threshold) + "\n")
+            f.write("duration: "+ str(end-start) + "\n")
+            f.write("\n")
+
+for i in range(len(big_G_list)):
+    with open((list(large_graph_dir.keys())[i])+"_random_size_results.txt", 'w') as f:
+        f.write("#############################\n")
+        f.write(list(large_graph_dir.keys())[i] + "/ graphs\n")
+        f.write("#############################\n")
+        f.write("\n")
+        for G in big_G_list[i]:
+            f.write("Graph {G} with {n} nodes and {e} edges.\n".format(G = G.graph['name'], n = len(G.nodes), e = len(G.edges)))
+            start = time.time()
+            sets_min, threshold = random_size_min_dom_sets(G, p = 3)
+            end = time.time()
+            for D in sets_min:
+                for v in D:
+                    f.write(str(v)+",")
+                f.write("\n")
+            f.write("threshold reached: "+ str(threshold) + "\n")
+            f.write("duration: "+ str(end-start) + "\n")
+            f.write("\n")
+
+for i in range(len(big_G_list)):
+    with open((list(large_graph_dir.keys())[i])+"_nx_results.txt", 'w') as f:
+        f.write("#############################\n")
+        f.write(list(large_graph_dir.keys())[i] + "/ graphs\n")
+        f.write("#############################\n")
+        f.write("\n")
+        for G in big_G_list[i]:
+            f.write("Graph {G} with {n} nodes and {e} edges.\n".format(G = G.graph['name'], n = len(G.nodes), e = len(G.edges)))
+            start = time.time()
+            sets_min, threshold = nx_min_dom_sets(G, p = 3)
+            end = time.time()
+            for D in sets_min:
+                for v in D:
+                    f.write(str(v)+",")
+                f.write("\n")
+            f.write("threshold reached: "+ str(threshold) + "\n")
+            f.write("duration: "+ str(end-start) + "\n")
+            f.write("\n")
+
+####
+# Now to run a small graph several times for statistical purposes
+####
+
+G = read_Graph(cwdir+"/SW_Graphs/SWmediumEWD.txt", form='SW', size_check=False, name="SWmediumEWD.txt")
+n = 1000
+with open("SWmediumEWD_results.txt", 'w') as f:
+    f.write("Statistical results for {n} runs.\n\n".format(n=n))
+    f.write("random_nodes_min_dom_sets(G, p= 20)\nthreshold\tduration\n")
+    for i in range(n):
+        start = time.time()
+        sets_min, threshold = random_nodes_min_dom_sets(G, p = 20)
+        end = time.time()
+        f.write("{threshold}\t{duration}\n".format(threshold=threshold, duration = end-start))
+        
+    f.write("random_size_min_dom_sets(G, p= 20)\nthreshold\tduration\n")    
+    for i in range(n):
+        start = time.time()
+        sets_min, threshold = random_size_min_dom_sets(G, p = 20)
+        end = time.time()
+        f.write("{threshold}\t{duration}\n".format(threshold=threshold, duration = end-start))
+        
+    f.write("nx_min_dom_sets(G, p= 20)\nthreshold\tduration\n")    
+    for i in range(n):
+        start = time.time()
+        sets_min, threshold = nx_min_dom_sets(G, p = 20)
+        end = time.time()
+        f.write("{threshold}\t{duration}\n".format(threshold=threshold, duration = end-start))
